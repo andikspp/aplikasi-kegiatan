a55a9ad5cfd2311782380c8a66bc4049f1a32e26
andikspp <andhika2003.ap31@gmail.com>
2024-11-25 10:45:21 +0700

    commit

--- resources/views/admin/menu/kelolapeserta.blade.php
+++ resources/views/admin/menu/kelolapeserta.blade.php
@@ -63,7 +63,8 @@
                 </div>
                 <div class="mt-3">
                     <label for="search" class="form-label">Search:</label>
-                    <input type="text" id="search" class="form-control" placeholder="Cari peserta..." oninput="applySearch()">
+                    <input type="text" id="search" class="form-control" placeholder="Cari peserta..."
+                        oninput="applySearch()">
                 </div>
             </div>
 
@@ -140,8 +141,10 @@
                                     @endif
                                 </td>
                                 <td>
-                                    <a href="{{ route('kegiatan.edit-peserta', $p->id) }}" class="btn btn-sm btn-warning">Edit</a>
-                                    <button type="button" class="btn btn-sm btn-danger" onclick="confirmDelete('{{ $p->id }}')">Hapus</button>
+                                    <a href="{{ route('kegiatan.edit-peserta', $p->id) }}"
+                                        class="btn btn-sm btn-warning">Edit</a>
+                                    <button type="button" class="btn btn-sm btn-danger"
+                                        onclick="confirmDelete('{{ $p->id }}')">Hapus</button>
                                 </td>
                             </tr>
                         @empty
@@ -203,41 +206,101 @@
         kabupatenSelect.innerHTML = '<option value="">Pilih Kabupaten</option>'; // Reset kabupaten
 
         const kabupatenData = {
-            aceh: ['Aceh Besar', 'Bireuen', 'Gayo Lues', 'Langsa', 'Lhokseumawe', 'Nagan Raya', 'Pidie', 'Simeulue'],
+            aceh: ['Aceh Besar', 'Bireuen', 'Gayo Lues', 'Langsa', 'Lhokseumawe', 'Nagan Raya', 'Pidie',
+                'Simeulue'
+            ],
             bali: ['Badung', 'Bangli', 'Buleleng', 'Gianyar', 'Jembrana', 'Karangasem', 'Kota Denpasar', 'Tabanan'],
             banten: ['Banten', 'Tangerang', 'Tangerang Selatan', 'Serang', 'Cilegon', 'Lebak', 'Pandeglang'],
-            bengkulu: ['Bengkulu', 'Bengkulu Selatan', 'Bengkulu Utara', 'Kaur', 'Seluma', 'Rejang Lebong', 'Lebong'],
+            bengkulu: ['Bengkulu', 'Bengkulu Selatan', 'Bengkulu Utara', 'Kaur', 'Seluma', 'Rejang Lebong',
+                'Lebong'
+            ],
             di_yogyakarta: ['Bantul', 'Gunungkidul', 'Kulon Progo', 'Sleman', 'Yogyakarta'],
             dki_jakarta: ['Jakarta Barat', 'Jakarta Pusat', 'Jakarta Selatan', 'Jakarta Timur', 'Jakarta Utara'],
-            jateng: ['Banyumas', 'Batang', 'Blora', 'Boyolali', 'Brebes', 'Demak', 'Grobogan', 'Jepara', 'Karanganyar', 'Kebumen', 'Klaten', 'Kudus', 'Pati', 'Pemalang', 'Purbalingga', 'Semarang', 'Sragen', 'Sukoharjo', 'Tegal', 'Wonogiri', 'Wonosobo'],
-            jatim: ['Bangkalan', 'Banyuwangi', 'Blitar', 'Bojonegoro', 'Bondowoso', 'Gresik', 'Jember', 'Kediri', 'Lamongan', 'Madiun', 'Magetan', 'Malang', 'Mojokerto', 'Nganjuk', 'Ngawi', 'Pamekasan', 'Pasuruan', 'Ponorogo', 'Sampang', 'Sidoarjo', 'Situbondo', 'Trenggalek', 'Tuban', 'Tulungagung'],
-            jabar: ['Bandung', 'Bandung Barat', 'Bekasi', 'Bogor', 'Ciamis', 'Cianjur', 'Cirebon', 'Garut', 'Indramayu', 'Karawang', 'Kuningan', 'Majalengka', 'Purwakarta', 'Subang', 'Sukabumi', 'Sumedang', 'Tasikmalaya'],
-            kalbar: ['Bengkayang', 'Kapuas Hulu', 'Kubu Raya', 'Landak', 'Melawi', 'Mempawah', 'Pontianak', 'Sambas', 'Sanggau', 'Sekadau', 'Singkawang', 'Sintang'],
-            kalsel: ['Banjar', 'Barito Kuala', 'Hulu Sungai Selatan', 'Hulu Sungai Tengah', 'Hulu Sungai Utara', 'Kotabaru', 'Tabalong', 'Tanah Bumbu', 'Tanah Laut', 'Barito Selatan', 'Barito Utara'],
-            kalteng: ['Barito Selatan', 'Barito Utara', 'Gunung Mas', 'Kapuas', 'Katingan', 'Kotawaringin Barat', 'Kotawaringin Timur', 'Lamandau', 'Murung Raya', 'Pulang Pisau', 'Seruyan', 'Sukamara'],
-            kaltim: ['Balikpapan', 'Bontang', 'Kutai Barat', 'Kutai Kartanegara', 'Kutai Timur', 'Mahakam Ulu', 'Paser', 'Penajam Paser Utara', 'Samarinda'],
-            kep_bangka_belitung: ['Bangka', 'Bangka Selatan', 'Bangka Tengah', 'Bangka Barat', 'Belitung', 'Belitung Timur'],
-            kepulauan_riau: ['Bintan', 'Karimun', 'Kota Batam', 'Kota Tanjung Pinang', 'Lingga', 'Natuna', 'Anambas'],
-            lampung: ['Bandar Lampung', 'Lampung Selatan', 'Lampung Tengah', 'Lampung Utara', 'Tanggamus', 'Way Kanan', 'Pesawaran', 'Pringsewu', 'Tulang Bawang', 'Tulang Bawang Barat', 'Metro'],
-            maluku: ['Maluku Tengah', 'Maluku Tenggara', 'Buru', 'Buru Selatan', 'Seram Bagian Barat', 'Seram Bagian Timur', 'Kota Ambon'],
-            maluku_utara: ['Halmahera Barat', 'Halmahera Selatan', 'Halmahera Tengah', 'Halmahera Utara', 'Kota Ternate', 'Kota Tidore Kepulauan'],
-            ntb: ['Bima', 'Dompu', 'Lombok Barat', 'Lombok Tengah', 'Lombok Timur', 'Sumbawa', 'Sumbawa Barat', 'Kota Mataram'],
-            ntt: ['Alor', 'Ende', 'Flores Timur', 'Kupang', 'Lembata', 'Manggarai', 'Manggarai Barat', 'Nagekeo', 'Ngada', 'Sikka', 'Sumba Barat', 'Sumba Timur', 'Timor Tengah Selatan', 'Timor Tengah Utara', 'Kota Kupang'],
-            papua: ['Boven Digoel', 'Jayapura', 'Jayawijaya', 'Kepulauan Yapen', 'Lanny Jaya', 'Mamberamo Raya', 'Mimika', 'Nabire', 'Paniai', 'Pegunungan Bintang', 'Sarmi', 'Supiori', 'Yahukimo', 'Yalimo'],
-            papua_barat: ['Fakfak', 'Kaimana', 'Manokwari', 'Maybrat', 'Pegunungan Arfak', 'Sorong', 'Sorong Selatan', 'Teluk Bintuni', 'Teluk Wondama'],
-            riau: ['Bengkalis', 'Dumai', 'Indragiri Hilir', 'Indragiri Hulu', 'Kampar', 'Kuantan Singingi', 'Pelalawan', 'Rokan Hilir', 'Rokan Hulu', 'Siak'],
+            jateng: ['Banyumas', 'Batang', 'Blora', 'Boyolali', 'Brebes', 'Demak', 'Grobogan', 'Jepara',
+                'Karanganyar', 'Kebumen', 'Klaten', 'Kudus', 'Pati', 'Pemalang', 'Purbalingga', 'Semarang',
+                'Sragen', 'Sukoharjo', 'Tegal', 'Wonogiri', 'Wonosobo'
+            ],
+            jatim: ['Bangkalan', 'Banyuwangi', 'Blitar', 'Bojonegoro', 'Bondowoso', 'Gresik', 'Jember', 'Kediri',
+                'Lamongan', 'Madiun', 'Magetan', 'Malang', 'Mojokerto', 'Nganjuk', 'Ngawi', 'Pamekasan',
+                'Pasuruan', 'Ponorogo', 'Sampang', 'Sidoarjo', 'Situbondo', 'Trenggalek', 'Tuban', 'Tulungagung'
+            ],
+            jabar: ['Bandung', 'Bandung Barat', 'Bekasi', 'Bogor', 'Ciamis', 'Cianjur', 'Cirebon', 'Garut',
+                'Indramayu', 'Karawang', 'Kuningan', 'Majalengka', 'Purwakarta', 'Subang', 'Sukabumi',
+                'Sumedang', 'Tasikmalaya'
+            ],
+            kalbar: ['Bengkayang', 'Kapuas Hulu', 'Kubu Raya', 'Landak', 'Melawi', 'Mempawah', 'Pontianak',
+                'Sambas', 'Sanggau', 'Sekadau', 'Singkawang', 'Sintang'
+            ],
+            kalsel: ['Banjar', 'Barito Kuala', 'Hulu Sungai Selatan', 'Hulu Sungai Tengah', 'Hulu Sungai Utara',
+                'Kotabaru', 'Tabalong', 'Tanah Bumbu', 'Tanah Laut', 'Barito Selatan', 'Barito Utara'
+            ],
+            kalteng: ['Barito Selatan', 'Barito Utara', 'Gunung Mas', 'Kapuas', 'Katingan', 'Kotawaringin Barat',
+                'Kotawaringin Timur', 'Lamandau', 'Murung Raya', 'Pulang Pisau', 'Seruyan', 'Sukamara'
+            ],
+            kaltim: ['Balikpapan', 'Bontang', 'Kutai Barat', 'Kutai Kartanegara', 'Kutai Timur', 'Mahakam Ulu',
+                'Paser', 'Penajam Paser Utara', 'Samarinda'
+            ],
+            kep_bangka_belitung: ['Bangka', 'Bangka Selatan', 'Bangka Tengah', 'Bangka Barat', 'Belitung',
+                'Belitung Timur'
+            ],
+            kepulauan_riau: ['Bintan', 'Karimun', 'Kota Batam', 'Kota Tanjung Pinang', 'Lingga', 'Natuna',
+                'Anambas'
+            ],
+            lampung: ['Bandar Lampung', 'Lampung Selatan', 'Lampung Tengah', 'Lampung Utara', 'Tanggamus',
+                'Way Kanan', 'Pesawaran', 'Pringsewu', 'Tulang Bawang', 'Tulang Bawang Barat', 'Metro'
+            ],
+            maluku: ['Maluku Tengah', 'Maluku Tenggara', 'Buru', 'Buru Selatan', 'Seram Bagian Barat',
+                'Seram Bagian Timur', 'Kota Ambon'
+            ],
+            maluku_utara: ['Halmahera Barat', 'Halmahera Selatan', 'Halmahera Tengah', 'Halmahera Utara',
+                'Kota Ternate', 'Kota Tidore Kepulauan'
+            ],
+            ntb: ['Bima', 'Dompu', 'Lombok Barat', 'Lombok Tengah', 'Lombok Timur', 'Sumbawa', 'Sumbawa Barat',
+                'Kota Mataram'
+            ],
+            ntt: ['Alor', 'Ende', 'Flores Timur', 'Kupang', 'Lembata', 'Manggarai', 'Manggarai Barat', 'Nagekeo',
+                'Ngada', 'Sikka', 'Sumba Barat', 'Sumba Timur', 'Timor Tengah Selatan', 'Timor Tengah Utara',
+                'Kota Kupang'
+            ],
+            papua: ['Boven Digoel', 'Jayapura', 'Jayawijaya', 'Kepulauan Yapen', 'Lanny Jaya', 'Mamberamo Raya',
+                'Mimika', 'Nabire', 'Paniai', 'Pegunungan Bintang', 'Sarmi', 'Supiori', 'Yahukimo', 'Yalimo'
+            ],
+            papua_barat: ['Fakfak', 'Kaimana', 'Manokwari', 'Maybrat', 'Pegunungan Arfak', 'Sorong',
+                'Sorong Selatan', 'Teluk Bintuni', 'Teluk Wondama'
+            ],
+            riau: ['Bengkalis', 'Dumai', 'Indragiri Hilir', 'Indragiri Hulu', 'Kampar', 'Kuantan Singingi',
+                'Pelalawan', 'Rokan Hilir', 'Rokan Hulu', 'Siak'
+            ],
             sulawesi_barat: ['Majene', 'Mamasa', 'Mamuju', 'Polewali Mandar'],
-            sulawesi_selatan: ['Bantaeng', 'Barru', 'Bone', 'Bulukumba', 'Enrekang', 'Gowa', 'Jeneponto', 'Luwu', 'Luwu Utara', 'Luwu Timur', 'Makassar', 'Maros', 'Pangkep', 'Pinrang', 'Soppeng', 'Takalar', 'Tana Toraja', 'Toraja Utara', 'Wajo'],
-            sulawesi_tengah: ['Banggai', 'Banggai Kepulauan', 'Buol', 'Donggala', 'Morowali', 'Palu', 'Poso', 'Sigi', 'Tojo Una-Una', 'Toli-Toli'],
-            sulawesi_utara: ['Bolaang Mongondow', 'Bolaang Mongondow Selatan', 'Bolaang Mongondow Timur', 'Bolaang Mongondow Utara', 'Minahasa', 'Minahasa Selatan', 'Minahasa Utara', 'Sangihe', 'Sitaro', 'Sangihe', 'Talaud', 'Tomohon', 'Kota Manado'],
-            sumatera_barat: ['Agam', 'Dharmasraya', 'Kota Bukittinggi', 'Kota Padang', 'Kota Pariaman', 'Kota Sawahlunto', 'Kota Solok', 'Lima Puluh Kota', 'Padang Pariaman', 'Pasaman', 'Pasaman Barat', 'Sijunjung', 'Solok', 'Tanah Datar'],
-            sumatera_selatan: ['Banyuasin', 'Empat Lawang', 'Lahat', 'Muara Enim', 'OKU', 'OKU Selatan', 'OKU Timur', 'Palembang', 'Pagar Alam', 'Prabumulih', 'Musirawas', 'Musirawas Utara', 'Lahat'],
-            sumatera_utara: ['Asahan', 'Batubara', 'Dairi', 'Deli Serdang', 'Humbang Hasundutan', 'Karo', 'Labuhanbatu', 'Labuhanbatu Selatan', 'Labuhanbatu Utara', 'Langkat', 'Nias', 'Nias Barat', 'Nias Selatan', 'Nias Utara', 'Simalungun', 'Tapanuli Selatan', 'Tapanuli Utara', 'Tebing Tinggi', 'Kota Medan'],
+            sulawesi_selatan: ['Bantaeng', 'Barru', 'Bone', 'Bulukumba', 'Enrekang', 'Gowa', 'Jeneponto', 'Luwu',
+                'Luwu Utara', 'Luwu Timur', 'Makassar', 'Maros', 'Pangkep', 'Pinrang', 'Soppeng', 'Takalar',
+                'Tana Toraja', 'Toraja Utara', 'Wajo'
+            ],
+            sulawesi_tengah: ['Banggai', 'Banggai Kepulauan', 'Buol', 'Donggala', 'Morowali', 'Palu', 'Poso',
+                'Sigi', 'Tojo Una-Una', 'Toli-Toli'
+            ],
+            sulawesi_utara: ['Bolaang Mongondow', 'Bolaang Mongondow Selatan', 'Bolaang Mongondow Timur',
+                'Bolaang Mongondow Utara', 'Minahasa', 'Minahasa Selatan', 'Minahasa Utara', 'Sangihe',
+                'Sitaro', 'Sangihe', 'Talaud', 'Tomohon', 'Kota Manado'
+            ],
+            sumatera_barat: ['Agam', 'Dharmasraya', 'Kota Bukittinggi', 'Kota Padang', 'Kota Pariaman',
+                'Kota Sawahlunto', 'Kota Solok', 'Lima Puluh Kota', 'Padang Pariaman', 'Pasaman',
+                'Pasaman Barat', 'Sijunjung', 'Solok', 'Tanah Datar'
+            ],
+            sumatera_selatan: ['Banyuasin', 'Empat Lawang', 'Lahat', 'Muara Enim', 'OKU', 'OKU Selatan',
+                'OKU Timur', 'Palembang', 'Pagar Alam', 'Prabumulih', 'Musirawas', 'Musirawas Utara', 'Lahat'
+            ],
+            sumatera_utara: ['Asahan', 'Batubara', 'Dairi', 'Deli Serdang', 'Humbang Hasundutan', 'Karo',
+                'Labuhanbatu', 'Labuhanbatu Selatan', 'Labuhanbatu Utara', 'Langkat', 'Nias', 'Nias Barat',
+                'Nias Selatan', 'Nias Utara', 'Simalungun', 'Tapanuli Selatan', 'Tapanuli Utara',
+                'Tebing Tinggi', 'Kota Medan'
+            ],
         };
 
         if (kabupatenData[provinsi]) {
             kabupatenData[provinsi].forEach(kabupaten => {
-                kabupatenSelect.innerHTML += `<option value="${kabupaten.toLowerCase().replace(/ /g, "_")}">${kabupaten}</option>`;
+                kabupatenSelect.innerHTML +=
+                    `<option value="${kabupaten.toLowerCase().replace(/ /g, "_")}">${kabupaten}</option>`;
             });
         }
     }
@@ -247,7 +310,8 @@
         const rows = document.querySelectorAll('#pesertaBody tr');
 
         rows.forEach(row => {
-            const namaLengkap = row.cells[1].textContent.toLowerCase(); // Mengambil nama lengkap dari kolom kedua
+            const namaLengkap = row.cells[1].textContent
+                .toLowerCase(); // Mengambil nama lengkap dari kolom kedua
             if (namaLengkap.includes(searchValue)) {
                 row.style.display = '';
             } else {
@@ -265,7 +329,8 @@
             const rowProvinsi = row.getAttribute('data-provinsi');
             const rowKabupaten = row.getAttribute('data-kabupaten');
 
-            if ((provinsi === '' || rowProvinsi === provinsi) && (kabupaten === '' || rowKabupaten === kabupaten)) {
+            if ((provinsi === '' || rowProvinsi === provinsi) && (kabupaten === '' || rowKabupaten ===
+                    kabupaten)) {
                 row.style.display = '';
             } else {
                 row.style.display = 'none';
